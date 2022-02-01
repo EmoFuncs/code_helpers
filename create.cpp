cf0b74459b173b01e375619c154f47282285d284
EmoFuncs <448947198@qq.com>
2022-02-02 00:41:58 +0800

    add info.json

--- create.cpp
+++ create.cpp
@@ -14,6 +14,7 @@ int main(int argc, char *argv[]) {
 	string problem_root = "problem_" + problem_id;
 	string problem_cpp = problem_root + "/" + problem_id + ".cpp";
 	string problem_in = problem_root + "/" + problem_id + ".in";
+	string problem_info = problem_root + "/" + "info.json";
 
 	if (!filesystem::exists(problem_root)) {
 		cmd.execute("mkdir", problem_root);
@@ -25,6 +26,10 @@ int main(int argc, char *argv[]) {
 	
 	if (!filesystem::exists(problem_in)) {
 		cmd.execute("touch", problem_in);
+	}
+
+	if (!filesystem::exists(problem_info)) {
+		cmd.execute("cp", "~/code/programming-contest-codes/_info.json", problem_info);
 	}
 	
 	return 0;
